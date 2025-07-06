eb005a4a0d4f1e74af1353241e47f6cb47acdfb9
mjshiremath <mrityunjaya.h@thoughtworks>
2025-07-06 21:42:49 +0530

    Update the eks name

--- network.tf
+++ network.tf
@@ -19,6 +19,15 @@ resource "aws_subnet" "eks_public_subnet_a" {
   }
 }
 
+resource "aws_subnet" "eks_public_subnet_b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "eks-public-subnet-b"
+  }
+}
 
 # Create private subnets for EKS
 resource "aws_subnet" "eks_private_subnet_a" {
@@ -31,6 +40,15 @@ resource "aws_subnet" "eks_private_subnet_a" {
   }
 }
 
+resource "aws_subnet" "eks_private_subnet_b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "eks-private-subnet-b"
+  }
+}
 
 # Create an Internet Gateway
 resource "aws_internet_gateway" "eks_igw" {
@@ -55,5 +73,10 @@ resource "aws_route_table" "eks_public_rt" {
 # Associate subnets with the route table
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.eks_public_subnet_a.id
+  route_table_id = aws_route_table.eks_public_rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.eks_public_subnet_b.id
   route_table_id = aws_route_table.eks_public_rt.id
 }
