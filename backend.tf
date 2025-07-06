780a3b0fbb1c1b259abf06b9f7f40dc32d8b5851
mjshiremath <mrityunjaya.h@thoughtworks>
2025-07-06 21:20:29 +0530

    output the region in backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "atlanta-dream"
-    key            = "aifsd/state/"
+    key            = "/aifsd/state/"
     region         = "us-east-1"
   }
 }
